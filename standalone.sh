55fa20c414f5d7585775bc0433203b22d1fe278c
Tristan F <LeoDog896@hotmail.com>
2022-10-04 13:31:07 -0400

    Update standalone.sh

--- standalone.sh
+++ standalone.sh
@@ -6,3 +6,5 @@ curl -fsSL https://deno.land/x/install@v0.1.7/install.sh | sh
 /home/gitpod/.deno/bin/deno completions bash > /home/gitpod/.bashrc.d/90-deno
 echo 'export DENO_INSTALL="/home/gitpod/.deno"' >> /home/gitpod/.bashrc.d/90-deno
 echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> /home/gitpod/.bashrc.d/90-deno
+
+printf "\033[42mDone!\033[0m Restart your terminal.\n"
